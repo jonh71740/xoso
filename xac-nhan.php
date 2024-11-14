0029f1849c8a71d3db7af918df4927b8662066e5
Hieu Ha <hahieu1008@gmail.com>
2024-11-14 18:49:35 +0700

    them page xac nhan

---
+++ xac-nhan.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- End Required meta tags -->
+    <!-- Begin SEO tag -->
+    <title> Xác nhận </title>
+    <!-- End SEO tag -->
+    <!-- Favicons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/apple-touch-icon.png">
+    <link rel="shortcut icon" href="assets/favicon.ico">
+    <meta name="theme-color" content="#3063A0">
+    <!-- BEGIN BASE STYLES -->
+    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/vendor/font-awesome/css/fontawesome-all.min.css">
+    <!-- END BASE STYLES -->
+    <!-- BEGIN THEME STYLES -->
+    <link rel="stylesheet" href="assets/stylesheets/main.min.css">
+    <link rel="stylesheet" href="assets/stylesheets/custom.css">
+    <!-- END THEME STYLES -->
+  </head>
+  <style>
+    @media (max-width: 600px) {
+      .card-xac-nhan{
+        width: 96% !important;
+        margin: 8px !important;
+        font-size: 12px;
+      }
+      .card-xac-nhan li {
+        margin: 10px 0;
+      }
+    }
+
+    @media (min-width: 600px) {
+      .card-xac-nhan{
+        font-size: 15px;
+      }
+      .card-xac-nhan li {
+        margin: 20px 0;
+      }
+      .card-xac-nhan ol{
+        padding-left: 14px;
+      }
+    }
+
+
+    .card-xac-nhan ol li.noted {
+        color: #d50137;
+        font-weight: 500;
+        position: relative;
+        width: -webkit-fit-content;
+        width: -moz-fit-content;
+        width: fit-content;
+    }
+    
+  </style>
+  <body>
+    <!-- .auth -->
+    <main class="auth">
+      <div class="card w-75 mt-3 card-xac-nhan">
+        <h5 class="card-header text-center" style="font-size: 30px; text-transform: uppercase;">Quy tắc đặt cược</h5>
+        <div class="card-body">
+          <ol>
+            <li> Có thể luật của nơi bạn đang sống có quy định rằng cá cược trên mạng là bất hợp pháp. Nếu thật sự xảy ra tình trạng này, Công ty chúng tôi sẽ không chịu trách nhiệm về những sự cố mà khách hàng gặp phải. </li>
+            <li> Nếu khách hàng nghi ngờ rằng dữ liệu cùa mình bị đánh cắp, nên thông báo ngay cho Đại lý cấp trên hoặc Văn phòng Công ty để được hỗ trợ kịp thời tránh những thiệt hại đáng tiếc. </li>
+            <li> Khách hàng có trách nhiệm bảo mật về tài khoản và thông tin đăng nhập của mình. Đối với những cá cược trực tuyến bằng tên người sử dụng và mật khẩu hợp lệ, thì Công ty sẽ coi là có hiệu lực. </li>
+            <li> Khi phát hành tỷ lệ cược mà xảy ra bất kỳ lỗi đánh máy hoặc lỗi không chủ ý của con người, bất kể trước hoặc sau khi có kết quả mở thưởng, Công ty có quyền xoá hoặc huỷ bỏ những mã cược bị lỗi ở bất cứ lúc nào và thông báo qua Thông báo điện tử (Công ty sẽ không thông báo cho từng cá nhân). </li>
+            <li> Mỗi lần đăng nhập khách hàng nên kiểm tra số dư tài khoản của mình. Nếu có bất kỳ vấn đề về số dư tài khoản, xin hãy thông báo cho Đại lý cấp trên hoặc Văn phòng Công ty ngay lập tức. </li>
+            <li> Một khi đặt cược được chấp nhận (đã qua thời gian hủy đặt cược), người chơi sẽ không thể sửa hay hủy bỏ các mã đặt cược. </li>
+            <li> Tỷ lệ cược của các con số sẽ được biến đổi thường xuyên, Khi phát thưởng sẽ áp dụng theo tỷ lệ cược mà ban đầu bạn đã đặt cược. </li>
+            <li> Tất cả các cá cược đều phải tiến hành trước thời gian mở thưởng Xổ Số nếu không các mã cược đó sẽ được xem là không hợp lệ. </li>
+            <li class="noted">Công ty có quyền từ chối, hủy những mã đặt cược bất thường hoặc có biểu hiện gian lận. <b> Xin lưu ý!</b>
+            </li>
+          </ol>
+        </div>
+        <footer class="card-footer">
+          <div class="card-footer-content text-muted" > <a href="/"> <button type="button" class="btn btn-success" style="float: right;">Tôi đồng ý</button></a> </div>
+        </footer>
+      </div>
+    </main>
+    <!-- /.auth -->
+    <!-- BEGIN PLUGINS JS -->
+    <script src="assets/vendor/particles.js/particles.min.js"></script>
+  </body>
+</html>
